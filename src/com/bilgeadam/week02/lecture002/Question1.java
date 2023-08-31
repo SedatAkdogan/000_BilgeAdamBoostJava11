4913384d4daecca67e306ce6f5fb84af5070f91d
Sedat <Sedat@DESKTOP-GOL7N5F>
2023-09-01 00:21:29 +0300

    Question

---
+++ src/com/bilgeadam/week02/lecture002/Question1.java
@@ -0,0 +1,14 @@
+package com.bilgeadam.week02.lecture002;
+
+public class Question1 {
+
+	public static void main(String[] args) {
+
+		int number1 = 30;
+		double number2 = 8;
+		double result = number1 / number2;
+		System.out.println(result);
+
+	}
+
+}
